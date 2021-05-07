9a9d44fd45b7b7f661ffde2f2726b9550ab80444
alo-beep <lopeza1829@gmail.com>
2021-05-07 12:13:14 -0700

    Only one marker at at time, added button functionality, and started second screen

--- app/src/main/java/alopez188/finalproject/MainActivity.java
+++ app/src/main/java/alopez188/finalproject/MainActivity.java
@@ -3,14 +3,23 @@ package alopez188.finalproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -20,19 +29,19 @@ import com.mapbox.mapboxsdk.maps.Style;
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
-    private Point pinPosition;
     private Marker marker;
     private Button btn_showWeather;
+    private MapboxMap mapboxMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
-
         setContentView(R.layout.activity_main);
 
         btn_showWeather = findViewById(R.id.btn_showWeather);
+        PinLocation pinLocation = new PinLocation();
 
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
@@ -46,16 +55,27 @@ public class MainActivity extends AppCompatActivity {
 
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
+                        // set camera centered in bakersfield
+                        CameraPosition position = new CameraPosition.Builder()
+                                .target(new LatLng(35.3733333, -119.0177778))
+                                .zoom(5)
+                                .tilt(20)
+                                .build();
+                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 10);
+
                         // gather longitude and latitude from point click
                         mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
                             @Override
                             public boolean onMapClick(@NonNull LatLng point) {
+                                mapboxMap.clear();
                                 Toast.makeText(MainActivity.this, String.format("User clicked at: %s", point.toString()), Toast.LENGTH_LONG).show();
                                 marker = mapboxMap.addMarker(new MarkerOptions().position(point));
 
-                                pinPosition = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+                                pinLocation.setLatitude(point.getLatitude());
+                                pinLocation.setLongitude(point.getLongitude());
 
                                 btn_showWeather.setEnabled(true);
+                                btn_showWeather.setText("Show Weather at Pin");
                                 return true;
                             }
                         });
@@ -106,5 +126,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    public void openWeatherScreen(View view) {
+        if (btn_showWeather.isEnabled() == true) {
+            startActivity(new Intent(MainActivity.this, WeatherActivity.class));
+        }
     }
 }
